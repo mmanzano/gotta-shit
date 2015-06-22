e5918fb675ba83ffcf1241ffed7668c37743f3e3
Miguel Manzano <mmanzano@gmail.com>
2015-06-22 18:04:13 +0200

    Testing: Initial

--- tests/HomeTest.php
+++ tests/HomeTest.php
@@ -4,16 +4,17 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class HomeTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function test_home()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->see('Shit Guide');
     }
+
 }
