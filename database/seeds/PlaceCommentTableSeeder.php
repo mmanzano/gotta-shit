ba026f1b428b9dbe4fdb804fc7ad56611a5f47aa
Miguel Manzano <mmanzano@gmail.com>
2015-07-24 11:11:06 +0200

    Faker reference cleaned in Seeders

--- database/seeds/PlaceCommentTableSeeder.php
+++ database/seeds/PlaceCommentTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class PlaceCommentTableSeeder extends Seeder
 {
--- database/seeds/PlaceStarTableSeeder.php
+++ database/seeds/PlaceStarTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class PlaceStarTableSeeder extends Seeder
 {
