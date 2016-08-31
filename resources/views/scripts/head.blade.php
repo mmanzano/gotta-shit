04576401ebb5e3700cf0a94d66776ecfc1aba99b
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-08-31 13:40:44 +0200

    Google JS API KEY

--- resources/views/scripts/head.blade.php
+++ resources/views/scripts/head.blade.php
@@ -8,6 +8,6 @@
     {{ trans('gottashit.site_name') }}
 </title>
 <link href="{{ asset('/css/gottashit.css') }}" rel="stylesheet">
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_JS_KEY') }}&v=3.exp&libraries=places"></script>
 
 
