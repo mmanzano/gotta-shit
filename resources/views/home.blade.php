79082efa2f9a62672824a485efb755e74380b013
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-11-18 16:37:44 +0100

    Some fun video in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="home">
         <div class="home-help">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/W2DAxnl2ppA" frameborder="0" allowfullscreen></iframe>
             {!! trans('gottashit.home') !!}
         </div>
         <div class='home-places'>
