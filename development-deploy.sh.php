ef6282a7e167271118092d183bf41caadcdd3d91
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2018-03-23 08:01:34 +0100

    Update travis recipe and readme file

--- development-deploy.sh.php
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-composer update
-chmod -R 777 storage
-
-php artisan key:generate
-php artisan migrate:refresh --seed
-php artisan view:clear
-php artisan route:clear
-php artisan cache:clear
